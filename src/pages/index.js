aa846daa27e48dc8a91d115ce4eef56f8be9508d
Suraj Pun <s_pun@outlook.com>
2022-12-03 02:25:44 +0000

    Added navBar

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,19 @@ import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
 
+// Import Components
+import NavBar from "../components/NavBar";
+
 export default function Home() {
   return (
     <>
       <Head>
         <title>The Slightly Curious Studio</title>
       </Head>
+
+      <header>
+        <NavBar />
+      </header>
 
       <main>
         <h1>The Slightly Curious</h1>
