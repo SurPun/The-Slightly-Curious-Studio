0b775b39076acc85c9e81066ce03417d1333ac97
Suraj Pun <s_pun@outlook.com>
2022-12-03 01:15:17 +0000

    Project setup

---
+++ src/pages/index.js
@@ -0,0 +1,17 @@
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>The Slightly Curious Studio</title>
+      </Head>
+
+      <main>
+        <h1>The Slightly Curious</h1>
+      </main>
+    </>
+  );
+}
