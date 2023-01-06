e84c1616817b19747c06f5b87852152d8a923b5a
Suraj <S_pun@outlook.com>
2023-01-06 15:40:38 +0000

    Restructure Project

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,6 @@
-import Head from "next/head";
-import Image from "next/image";
-import Link from "next/link";
-
-// Import Components
-import NavBar from "../components/NavBar";
-
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>The Slightly Curious Studio</title>
-      </Head>
-
-      <header>
-        <NavBar />
-      </header>
-
       <main>
         <h1>The Slightly Curious</h1>
       </main>
